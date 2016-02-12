636b38743f02b7bd09f18c81ae6cb0e9622582ba
Santtu Sulander <santtu@sulander.com>
2016-02-12 02:27:38 -0500

    modified server and dependencies

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ var port = 8000;
 
 express()
 	// Serve the 'dist' directory which holds built and compiled css and js
-	.use('/dist', express.static(path.join(__dirname, '../dist')))
+	.use('/dist', express.static(path.join(__dirname, './dist')))
 
 	//All get requests made will be met responded with index.html.
 	.get('*', (req, res) => {
-		return res.sendFile(path.join(__dirname, '../index.html'));
+		return res.sendFile(path.join(__dirname, './index.html'));
 	})
 
 	// Start the server at the given 'PORT'.
